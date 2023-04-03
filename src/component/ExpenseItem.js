5881c9ab6a263a58545cc1bcc453a2566cf3ed56
HalilSacirovic <halilsacirovic03@gmail.com>
2023-04-03 20:21:16 +0200

    Sometinh new

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,6 +1,19 @@
-function ExpenseItem(){
-    return <h2>Expense Item test!!!</h2>
-}
+import './ExpenseItem.css'
 
+function ExpenseItem() {
+    const expenseDate = new Date(2021, 10, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.69;
+
+    return (
+    <div className="expense-item">
+      <div>Oktobar 28th 2003</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$294.69</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
