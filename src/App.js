e034a932472979ebea245e25bb14b44e40acce4f
HalilSacirovic <halilsacirovic03@gmail.com>
2023-04-13 23:45:10 +0200

    new Item

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ const App = () => {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: 'e5',
+      title: 'Desktop Computer',
+      amount: 699,
+      date: new Date(2021, 5, 12),
+    },
   ];
 
   // return React.createElement(
